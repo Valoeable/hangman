6df310f148326e13e6884365917bdb28d7ab0950
Valoeable <samo.valo3@gmail.com>
2022-11-27 20:29:47 +0100

    Added class variables for letter availability and a main loop for the game

--- hangman.rb
+++ hangman.rb
@@ -2,8 +2,13 @@ class Hangman
 
     def initialize
     @num_of_guesses = 7
+    @guessed_word = false
+    @letters_available = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
+    @guessed_letters = []
     game
     end
+
+    attr_accessor :num_of_guesses, :guessed_word, :letters_available, :guessed_letters
 
     def pick_random_line
         lines = File.readlines('google-10000-english-no-swears.txt')
@@ -12,7 +17,27 @@ class Hangman
 
     def game
         random_word = pick_random_line
-        hidden_word = Array.new(random_word.length - 1, '_')
+        random_word = random_word.split('')
+        random_word.pop
+        hidden_word = Array.new(random_word.length, '_')
+        game_over_display = %w[H A N G M A N]
+
+        puts('Welcome to the game of Hangman!')
+
+        until @num_of_guesses.zero? || @guessed_word
+            puts('Select a letter which you think the word might contain:')
+            chosen_letter = gets.chomp
+            chosen_letter = chosen_letter.downcase
+
+            was_it_a_hit(chosen_letter)
+
+        end
+
+
+
+        def was_it_a_hit(letter)
+
+        end
 
     end
 
